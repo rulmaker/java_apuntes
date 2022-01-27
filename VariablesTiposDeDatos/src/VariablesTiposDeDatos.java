9482a2bbef8d4bb652765f8957e831e28960ad37
raul <rauljaciel_91@hotmail.com>
2022-01-27 02:17:56 -0600

    Primero hola mundo en java

---
+++ VariablesTiposDeDatos/src/VariablesTiposDeDatos.java
@@ -0,0 +1,25 @@
+
+public class VariablesTiposDeDatos {
+	public static void main(String[] args) {
+		String nombre = "Raul Sanchez";
+		int edad = 30;
+		
+		char inicial =  'D';
+		
+		boolean tienesCrush = true;
+		
+		byte edad1 = 31;
+		
+		short suma = 24 + 104;
+		
+		int numeroLargo = 1021321312;
+		
+		System.out.println(numeroLargo);
+		
+		float promedio = 1232.23232f;
+		
+		double numeroPi = 3.1415934324234;
+		
+		
+	}
+}
